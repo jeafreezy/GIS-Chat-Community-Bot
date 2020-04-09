0410006c55453958527da3c7e0ffaf83d6724c03
Jeafreezy <jolaiyaemmanuel@gmail.com>
2020-04-09 15:48:18 +0100

    Ignored all sensitive files

---
+++ config.py
@@ -0,0 +1,35 @@
+from os import getenv
+import tweepy
+
+
+# authentication using Twitter API
+
+def create_api():
+    """This function collects the consumer and access keys, creates an api object and returns the authenticated api
+    object.
+    :return: authenticated api object
+    """
+    consumer_key = getenv('CONSUMER_KEY')
+    consumer_secret = getenv('CONSUMER_SECRET')
+    access_token = getenv('KEY')
+    access_token_secret=getenv('ACCESS_TOKEN_SECRET')
+    authentication = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    authentication.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    return authenticate_api(api)
+
+
+def authenticate_api(api):
+    """This function verifies the credentials and returns the authenticated api object
+    :param api: api object
+    :return: authenticated api object
+    :raises TweepError: raised due to an error Twitter Responded with or raised when an API method fails due to hitting
+    Twitter’s rate limit.
+    """
+    try:
+        api.verify_credentials()
+    except tweepy.TweepError as error:
+        raise error
+    print("API is successfully created!")
+    return api
