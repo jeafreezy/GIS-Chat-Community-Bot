e5d3524a330756b90ea1686e9634002d910f57fd
Jeafreezy <jolaiyaemmanuel@gmail.com>
2020-04-09 16:02:19 +0100

    Updated config file

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def create_api():
     """
     consumer_key = getenv('CONSUMER_KEY')
     consumer_secret = getenv('CONSUMER_SECRET')
-    access_token = getenv('KEY')
+    access_token = getenv('ACCESS_TOKEN')
     access_token_secret=getenv('ACCESS_TOKEN_SECRET')
     authentication = tweepy.OAuthHandler(consumer_key, consumer_secret)
     authentication.set_access_token(access_token, access_token_secret)
