6ac254783f7865df4fa4068c2d9261e356436f03
Jeafreezy <jolaiyaemmanuel@gmail.com>
2020-04-10 23:20:06 +0100

    Added search file to follow followers and find keyword

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # importing the modules
 from os import getenv
 import tweepy
+import logging
 
 
+logger = logging.getLogger()
 # authentication using Twitter API
 # creating a function so that it can be used in other modules
 def create_api():
@@ -18,9 +20,7 @@ def create_api():
     authentication = tweepy.OAuthHandler(consumer_key, consumer_secret)
     authentication.set_access_token(access_token, access_token_secret)
     api = tweepy.API(authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
     return authenticate_api(api)
-
 
 def authenticate_api(api):
     """This function verifies the credentials and returns the authenticated api object
@@ -32,6 +32,15 @@ def authenticate_api(api):
     try:
         api.verify_credentials()
     except tweepy.TweepError as error:
+        logger.error("Error creating API", exc_info=True)
         raise error
     print("API is successfully created!")
+    logger.info("API created")
     return api
+
+#Getting Geo ID for Nigeria
+#api=create_api()
+#places = api.geo_search(query="Nigeria", granularity="country")
+#print(places[0].id)
+
+create_api()
